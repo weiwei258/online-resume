23f28ab0620d001acfce42ea073b81ff6e1bb065
leon <adaxiaxi@163.com>
2022-08-23 23:14:24 +0800

    'vite配置文件,element-ui按需引入配置'

--- vite.config.ts
+++ vite.config.ts
@@ -2,36 +2,35 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import styleImport from 'vite-plugin-style-import'
+import { createStyleImportPlugin, ElementPlusResolve } from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    styleImport({
-      libs: [
-        {
-          libraryName: 'element-plus',
-          esModule: true,
-          ensureStyleFile: true,
-          resolveStyle: (name) => {
-            return `element-plus/lib/theme-chalk/${name}.css`
-          },
-          resolveComponent: (name) => {
-            return `element-plus/lib/${name}`
+export default ({ mode }) =>
+  defineConfig({
+    plugins: [
+      vue(),
+      vueJsx(),
+      createStyleImportPlugin({
+        resolves: [ElementPlusResolve()],
+        libs: [
+          {
+            libraryName: 'element-plus',
+            esModule: true,
+            ensureStyleFile: true,
+            resolveStyle: (name) => {
+              return `element-plus/lib/theme-chalk/${name}.css`
+            }
           }
-        }
-      ]
-    })
-  ],
-  base: mode === 'development' ? '/' : './', //此时把环境打包路径也配置好，避免生产环境打包出现白屏
-  server: {
-    port: 8888
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, '/src')
+        ]
+      })
+    ],
+    base: mode === 'development' ? '/dist' : '/build', //此时把环境打包路径也配置好，避免生产环境打包出现白屏
+    server: {
+      port: 8888
+    },
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, '/src')
+      }
     }
-  }
-})
+  })
