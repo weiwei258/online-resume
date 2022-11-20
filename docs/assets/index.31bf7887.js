95993c552bbe4173d6ff878d3132f3889e34ce57
weiwei <dengweiwei@dengweiweideMacBook-Pro.local>
2022-11-20 11:19:13 +0800

    merge

--- docs/assets/index.31bf7887.js
+++
@@ -1,1 +0,0 @@
-import{k as e,a as t,b as o}from"./index.0e55ef32.js";const a=e({props:{message:String},setup(r){return()=>t("div",null,[o("404")])}});export{a as default};
--- docs/assets/index.4269fccb.js
+++
@@ -1,1 +0,0 @@
-import{k as e,a as o,b as t}from"./index.0e55ef32.js";const a=e({props:{message:String},setup(r){return()=>o("div",null,[t("login")])}});export{a as default};
